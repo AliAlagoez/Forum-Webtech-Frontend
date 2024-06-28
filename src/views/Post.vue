0a00805c33d6ded264486627ffdd89955fe8c507
AMinato77 <amin.tekaya@gmail.com>
2024-06-28 13:51:21 +0200

    Design

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Posts</h1>
-    <button type="button" @click="fetchMyPosts">My posts</button>
+    <button type="button" @click="toggleMyPosts">My posts</button>
     <button type="button" @click="fetchMyPostsFromRender">My comments</button>
     <h2>New Post</h2>
     <form @submit.prevent="submitPost">
@@ -10,16 +10,34 @@
       <input v-model="newPost.author" type="text" placeholder="Enter author" required />
       <button type="submit">Submit</button>
     </form>
-    <div v-if="posts.length">
+    <div v-if="showPosts && posts.length">
       <h2>My Posts:</h2>
-      <ul>
-        <li v-for="post in posts" :key="post.id">
-          <h3>{{ post.title }}</h3>
-          <p>{{ post.content }}</p>
-          <p><strong>Author:</strong> {{ post.author }}</p>
-          <p><strong>Created At:</strong> {{ new Date(post.createdAt).toLocaleString() }}</p>
-        </li>
-      </ul>
+      <table>
+        <thead>
+        <tr>
+          <th>Title</th>
+          <th>Content</th>
+          <th>Author</th>
+          <th>Created At</th>
+          <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td v-if="editPostId !== post.id">{{ post.title }}</td>
+          <td v-else><input v-model="editPost.title" type="text" /></td>
+          <td v-if="editPostId !== post.id">{{ post.content }}</td>
+          <td v-else><input v-model="editPost.content" type="text" /></td>
+          <td>{{ post.author }}</td>
+          <td>{{ new Date(post.createdAt).toLocaleString() }}</td>
+          <td>
+            <button v-if="editPostId !== post.id" @click="enableEditPost(post)">Edit</button>
+            <button v-else @click="saveEditPost(post.id)">Save</button>
+            <button v-if="editPostId === post.id" @click="cancelEditPost">Cancel</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -40,7 +58,13 @@ export default defineComponent({
         title: '',
         content: '',
         author: ''
-      }
+      },
+      editPost: {
+        title: '',
+        content: ''
+      },
+      editPostId: null, // New state to track the post being edited
+      showPosts: false // New state to track whether to show posts or not
     }
   },
   methods: {
@@ -77,6 +101,36 @@ export default defineComponent({
         .catch(error => {
           console.error('There was an error submitting the post!', error)
         })
+    },
+    toggleMyPosts () {
+      this.showPosts = !this.showPosts
+      if (this.showPosts && this.posts.length === 0) {
+        this.fetchMyPosts()
+      }
+    },
+    enableEditPost (post) {
+      this.editPostId = post.id
+      this.editPost.title = post.title
+      this.editPost.content = post.content
+    },
+    saveEditPost (postId) {
+      const url = `${API_BASE_URL}/${postId}` // Adjust URL based on your backend API
+      axios.put(url, this.editPost)
+        .then(response => {
+          console.log('Post updated!', response)
+          this.fetchMyPosts() // Fetch updated posts
+          this.editPostId = null
+          this.editPost.title = ''
+          this.editPost.content = ''
+        })
+        .catch(error => {
+          console.error('There was an error updating the post!', error)
+        })
+    },
+    cancelEditPost () {
+      this.editPostId = null
+      this.editPost.title = ''
+      this.editPost.content = ''
     }
   },
   mounted () {
@@ -86,4 +140,18 @@ export default defineComponent({
 </script>
 
 <style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #f2f2f2;
+}
 </style>
